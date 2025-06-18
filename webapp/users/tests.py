dc5d7b16fbab134f50e1df96a6406ea46549a4c5
zscbana <zscbana@gmail.com>
2025-06-19 01:04:13 +0300

    Add Jenkins pipeline for CI/CD
    Add Jenkinsfile for building and deploying Django app
    
    Add Jenkins pipeline to automate build and test process

--- webapp/users/tests.py
+++ webapp/users/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
 # Create your tests here.
+class AlwaysPassTest1(TestCase):
+    def test_true_is_true(self):
+        self.assertTrue(True)
+
+class AlwaysPassTest2(TestCase):
+    def test_true_is_true(self):
+        self.assertTrue(True)
+
+class AlwaysPassTest3(TestCase):
+    def test_true_is_true(self):
+        self.assertTrue(True)
+
+class AlwaysPassTest4(TestCase):
+    def test_true_is_true(self):
+        self.assertTrue(True)
+
+class AlwaysPassTest(TestCase):
+    def test_true_is_true(self):
+        self.assertTrue(True)
